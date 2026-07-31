4c64a0f0177e0255649a448b3b73d27fa99be983
Yuvraj <takcommunity99@gmail.com>
2026-07-31 18:55:36 +0530

    check_yaml

--- src/check_yaml.py
+++ src/check_yaml.py
@@ -1,7 +1,6 @@
 import yaml
 
-with open("semantic/schema_cards.yaml", encoding="utf-8") as f:
+with open("semantic/glossary.yaml", encoding="utf-8") as f:
     data = yaml.safe_load(f)
 
 print(data.keys())
-print(data["olist_orders_dataset"]["joins"])
